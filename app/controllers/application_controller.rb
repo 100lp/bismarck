5b89d4550a6e48abc5d443fb66ddf49cd55b65f8
100lp <spb.gurme@gmail.com>
2014-04-19 17:06:36 +0400

    back_next for @nav

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-  before_filter :mebel_paths, :current_page
+  before_filter :mebel_paths, :current_page, :back_next
 
   def mebel_paths
     @korpus = "korpus"
@@ -10,7 +10,11 @@ class ApplicationController < ActionController::Base
   end
 
   def current_page
-    @current_page = request.original_url
+    @current_page = request.env['PATH_INFO']
+  end
+
+  def back_next
+    @mebel_nav = %W[ kitchen cupboard lounge kids cloakroom hall bibble ]
   end
 
 end
